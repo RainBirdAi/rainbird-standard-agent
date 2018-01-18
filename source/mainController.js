e965cab5d5c72d1cc2024f02530819f8f639a96c
nroberts1 <nathan.roberts@rainbird.ai>
2018-01-18 09:57:06 +0000

    RB-1377 rework with latest develop changes

--- source/mainController.js
+++ source/mainController.js
@@ -1,9 +1,30 @@
 angular.module('rbAgent')
-    .controller('MainController', ['$scope', 'config', '$state',
-        function($scope, config, $state) {
+    .controller('MainController', ['$scope', 'config', '$state', '$timeout', '$rootScope',
+        function($scope, config, $state, $timeout, $rootScope) {
             $scope.config = config;
             $scope.state = $state;
             $state.go('main.goalList', null, {
                 location: 'replace'
             });
+
+
+
+            $scope.$watch('apiOutput', function() {
+                
+                if (agentMemory.tryGoal) {
+                    var displayText = JSON.stringify($rootScope.apiOutput, null, 1);
+                    
+                    var lines = displayText.split('\n');
+                    lines.splice(0,1);
+                    if (lines.length > 0) {
+                        lines.splice(lines.length - 1,1);
+                    }
+                    $rootScope.apiOutputDisplay = lines.join('\n');
+                }
+                $timeout(function() {
+                    var logPanel = document.querySelector('#scroll-content');
+                    logPanel.scrollTop = logPanel.scrollHeight;
+                }, 150);
+                
+            }, true);
         }]);
