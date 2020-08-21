a9045014ff021539dfffc36571698101ed42d0ba
David Rampling <davidrampling@davids-mbp.rainbird.local>
2020-08-21 09:12:52 +0100

    fix update service to run with query param

--- server.js
+++ server.js
@@ -1,46 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const request = require('superagent');
+const path = require("path");
+const request = require("superagent");
 
 //Proxies 'Evidence Tree' request to the Rainbird Community environment
-app.get('/applications/components/rainbird-analysis-ui/:whyAnalysis', function(req, res) {
-    res.redirect('https://app.rainbird.ai' + req.originalUrl);
+app.get("/applications/components/rainbird-analysis-ui/:whyAnalysis", function (
+  req,
+  res
+) {
+  res.redirect("https://app.rainbird.ai" + req.originalUrl);
 });
 
-app.use('/components', express.static(path.join(__dirname, 'components')));
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
-app.use('/applications', express.static(path.join(__dirname, 'dist')));
+app.use("/components", express.static(path.join(__dirname, "components")));
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+app.use("/applications", express.static(path.join(__dirname, "dist")));
 
-app.set('views', __dirname);
-app.set('view engine', 'ejs');
+app.set("views", __dirname);
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    //Contains a demo agent id
-    return res.render('agent', {
-        id: 'dbd8b876-56d6-4db0-acaa-0c250619e26d',
-        api: 'https://api.rainbird.ai',
-        syncToken: undefined,
-    });
+app.get("/", function (req, res) {
+  //Contains a demo agent id
+  return res.render("agent", {
+    id: "dbd8b876-56d6-4db0-acaa-0c250619e26d",
+    api: "https://api.rainbird.ai",
+    syncToken: undefined,
+    engine: "Core",
+  });
 });
 
 //Proxies 'config' request to the Rainbird Community environment
-app.get('/agent/:id/config', function(req, res) {
-    request.get('https://app.rainbird.ai/agent/' + req.params.id + '/config')
-        .end(function (err, response){
-            res.send(response.body);
-        });
+app.get("/agent/:id/config", function (req, res) {
+  request
+    .get(
+      "https://app.rainbird.ai/agent/" +
+        req.params.id +
+        "/config?engine=" +
+        req.query.engine || "Core"
+    )
+    .end(function (err, response) {
+      res.send(response.body);
+    });
 });
 
 //Proxies 'start' request to the Rainbird Community environment
-app.get('/agent/:id/start/contextId', function(req, res) {
-    request.get('https://app.rainbird.ai/agent/' + req.params.id + '/start')
-        .end(function (err, response){
-            res.send(response.body);
-        });
+app.get("/agent/:id/start/contextId", function (req, res) {
+  request
+    .get("https://app.rainbird.ai/agent/" + req.params.id + "/start")
+    .end(function (err, response) {
+      res.send(response.body);
+    });
 });
 
-app.listen(8080, function() {
-    console.log('Web server started on port: 8080.');
-    console.log('To run example \'Speaks\' goal, browse to: http://localhost:8080/');
+app.listen(8080, function () {
+  console.log("Web server started on port: 8080.");
+  console.log(
+    "To run example 'Speaks' goal, browse to: http://localhost:8080/"
+  );
 });
